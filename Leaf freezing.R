36fe484626eda4f7afd25768691db7a6db130b9f
Joe Endris <joseph.endris@outlook.com>
2022-12-08 10:11:45 -0600

    Update after working with Evan

--- Leaf freezing.R
+++ Leaf freezing.R
@@ -13,23 +13,44 @@ setwd("~/Library/CloudStorage/GoogleDrive-jendris@my.apsu.edu/.shortcut-targets-
 raw_data <- read.csv("scrap.csv")
 
 #create an unique ID for each tree species/number/temp/date
-freeze_data <- mutate(raw_data, Unique_ID = paste(Date.Collected, Spec,Num, Temp, sep = "."))
+freeze_data <- mutate(raw_data, Unique_ID = paste(Date.Collected, Spec,Num, sep = ""))
 
 #calculate leakage (initial leakage/final leakage) x 100
-leakage_percent <- mutate(freeze_data, leakage = ((adjusted_elec1/adjusted_elec2)*100))
+freeze_data<-mutate(freeze_data, leakage = ((adjusted_elec1/adjusted_elec2)*100))
 
-#calculate injury (leakage- leakage(control))/(100-leakage(control)) x 100
-#i need this to reference the appropriate control for each replicate
-injury_percent <- mutate (leakage_percent, injury = ((leakage-leakage_percent[1,13])/(100-leakage_percent[1,13])*100))
+#looping over each unique ID for injury
+comb<-data.frame()
+for(j in 1:length(unique(freeze_data$Unique_ID))){
+  id<-unique(freeze_data$Unique_ID)[j]
+  test<-freeze_data[which(freeze_data$Unique_ID==id),]
+  for(i in 1:6){
+test$injury[i]<-((test$leakage[i]-test$leakage[1])/(100-test$leakage[1]))*100
+}
+comb<-rbind(comb,test)
+}
+comb$injury<-as.numeric(comb$injury)
+#changing all negatives to zero before calculating the adjusted injury
+comb$injury<-ifelse(comb$injur<0,0,comb$injury)
+comb$adjInj<-0
 
-#force any negative numbers in injury to be positive
-injury_percent$injury <- ifelse (injury_percent$injury<0,0,injury_percent$injury)
+#change all -40 control results to 100 when the value in the injury column is 0
+#first need to inspect these individual data to see if entered wrong
+x<-which(comb$injury==0&comb$Temp==-40)
+comb$Unique_ID[x]
 
-#calculate adjusted injury (injury/injury(-40)) x 100
-adjusted_injury_percent <- mutate (injury_percent, adjusted_injury = (injury/injury_percent[6,14])*100)
+#still need to do the adjusted injury
+comb2<-data.frame()
+for(j in 1:length(unique(comb$Unique_ID))){
+  id<-unique(comb$Unique_ID)[j]
+  test<-comb[which(comb$Unique_ID==id),]
+  for(i in 1:6){
+    test$adjInj[i]<-((test$injury[i]/test$injury[6]))*100
+  }
+  comb2<-rbind(comb2,test)
+}
 
 #force any negative numbers in adjusted_injury to be positive
-adjusted_injury_percent$injury <- ifelse (adjusted_injury_percent$injury<0,0,adjusted_injury_percent$injury)
+comb2$adjInj<-round(comb2$adjInj,2)
 
 #Current Issues
 #I need to figure out how to loop back to appropriate controls for each replicate, not just call to a specific cell
