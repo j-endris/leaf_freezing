74eb016c18dd26e54954d5a7451615069fe15a84
Joe Endris <joseph.endris@outlook.com>
2022-12-08 08:47:25 -0600

    Add stuff

--- Leaf freezing.R
+++ Leaf freezing.R
@@ -23,13 +23,13 @@ leakage_percent <- mutate(freeze_data, leakage = ((adjusted_elec1/adjusted_elec2
 injury_percent <- mutate (leakage_percent, injury = ((leakage-leakage_percent[1,13])/(100-leakage_percent[1,13])*100))
 
 #force any negative numbers in injury to be positive
-injury_percent$injury -> ifelse (injury_percent$injury<0,0,injury_percent$injury)
+injury_percent$injury <- ifelse (injury_percent$injury<0,0,injury_percent$injury)
 
 #calculate adjusted injury (injury/injury(-40)) x 100
 adjusted_injury_percent <- mutate (injury_percent, adjusted_injury = (injury/injury_percent[6,14])*100)
 
 #force any negative numbers in adjusted_injury to be positive
-adjusted_injury_percent$injury -> if_else (adjusted_injury_percent$injury<0,0,adjusted_injury_percent$injury)
+adjusted_injury_percent$injury <- ifelse (adjusted_injury_percent$injury<0,0,adjusted_injury_percent$injury)
 
 #Current Issues
 #I need to figure out how to loop back to appropriate controls for each replicate, not just call to a specific cell
