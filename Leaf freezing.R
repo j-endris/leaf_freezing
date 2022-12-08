60a3ed94556bb1a4beef88d522285598b731a049
Joe Endris <joseph.endris@outlook.com>
2022-12-08 11:50:36 -0600

    changed the name from comb to reflect the calculation being conducted.

--- Leaf freezing.R
+++ Leaf freezing.R
@@ -19,46 +19,41 @@ freeze_data <- mutate(raw_data, Unique_ID = paste(Date.Collected, Spec,Num, sep 
 freeze_data<-mutate(freeze_data, leakage = ((adjusted_elec1/adjusted_elec2)*100))
 
 #looping over each unique ID for injury
-comb<-data.frame()
+injury_percent<-data.frame()
 for(j in 1:length(unique(freeze_data$Unique_ID))){
   id<-unique(freeze_data$Unique_ID)[j]
   test<-freeze_data[which(freeze_data$Unique_ID==id),]
   for(i in 1:6){
 test$injury[i]<-((test$leakage[i]-test$leakage[1])/(100-test$leakage[1]))*100
 }
-comb<-rbind(comb,test)
+injury_percent<-rbind(injury_percent,test)
 }
-comb$injury<-as.numeric(comb$injury)
+injury_percent$injury<-as.numeric(injury_percent$injury)
 #changing all negatives to zero before calculating the adjusted injury
-comb$injury<-ifelse(comb$injur<0,0,comb$injury)
-comb$adjInj<-0
+injury_percent$injury<-ifelse(injury_percent$injur<0,0,injury_percent$injury)
+injury_percent$adjInj<-0
 
 #change all -40 control results to 100 when the value in the injury column is 0
 #first need to inspect these individual data to see if entered wrong
-x<-which(comb$injury==0&comb$Temp==-40)
-comb$Unique_ID[x]
+x<-which(injury_percent$injury==0&injury_percent$Temp==-40)
+injury_percent$Unique_ID[x]
 
-#still need to do the adjusted injury
-comb2<-data.frame()
-for(j in 1:length(unique(comb$Unique_ID))){
-  id<-unique(comb$Unique_ID)[j]
-  test<-comb[which(comb$Unique_ID==id),]
+#calculate adjusted injury (Injury[temperature]-/injury[-40])x 100
+adj_injury_percent<-data.frame()
+for(j in 1:length(unique(injury_percent$Unique_ID))){
+  id<-unique(injury_percent$Unique_ID)[j]
+  test<-injury_percent[which(injury_percent$Unique_ID==id),]
   for(i in 1:6){
     test$adjInj[i]<-((test$injury[i]/test$injury[6]))*100
   }
-  comb2<-rbind(comb2,test)
+  adj_injury_percent<-rbind(adj_injury_percent,test)
 }
 
 #force any negative numbers in adjusted_injury to be positive
-comb2$adjInj<-round(comb2$adjInj,2)
+adj_injury_percent$adjInj<-round(adj_injury_percent$adjInj,2)
 
-#Current Issues
-#I need to figure out how to loop back to appropriate controls for each replicate, not just call to a specific cell
-#is this where I need to run a loop?
 
-#code for absolute numbers isn't working correctly; 
-#returns error Error in ifelse(injury_percent$injury < 0, 0, NA) <- injury_percent$injury : 
-#could not find function "ifelse<-"
+
 
 
 
