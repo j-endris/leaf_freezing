d1e72477f01a1e98a6fc75835392a229ed2fd543
Joe Endris <joseph.endris@outlook.com>
2022-12-09 16:50:36 -0600

    cleaned up errors once again from github being weird

--- Leaf freezing.R
+++ Leaf freezing.R
@@ -36,24 +36,16 @@ injury_percent$injury<-as.numeric(injury_percent$injury)
 injury_percent$injury<-ifelse(injury_percent$injur<0,0,injury_percent$injury)
 injury_percent$adjInj<-0
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 4e0fee72d5c2415e77539955ceb431d8d164728a
 #change all -40 control results to 100 when the value in the injury column is 0
 #first need to inspect these individual data to see if entered wrong
 x<-which(injury_percent$injury==0&injury_percent$Temp==-40)
 injury_percent$Unique_ID[x]
-<<<<<<< HEAD
 
 #force any negative numbers in injury to be positive
 injury_percent$injury <- ifelse(injury_percent$injury<0,0,injury_percent$injury)
-=======
-=======
+
 #force any negative numbers in injury to be positive
 injury_percent$injury <- ifelse(injury_percent$injury<0,0,injury_percent$injury)
->>>>>>> 89f3503bc424a2f0372892636ff027f4514d1eb9
->>>>>>> 4e0fee72d5c2415e77539955ceb431d8d164728a
 
 #calculate adjusted injury (Injury[temperature]-/injury[-40])x 100
 adj_injury_percent<-data.frame()
@@ -66,7 +58,7 @@ for(j in 1:length(unique(injury_percent$Unique_ID))){
   adj_injury_percent<-rbind(adj_injury_percent,test)
 }
 
-<<<<<<< HEAD
+#round the numbers in the adjusted injury column to two digits past decimal point
 adj_injury_percent$adjInj<-round(adj_injury_percent$adjInj,2)
 
 #force any negative numbers in adjusted_injury to be positive
@@ -74,29 +66,6 @@ adj_injury_percent$injury <- if_else (adj_injury_percent$injury<0,0,adj_injury_p
 
 
 
-
-=======
-#force any negative numbers in adjusted_injury to be positive
-<<<<<<< HEAD
-adj_injury_percent$adjInj<-round(adj_injury_percent$adjInj,2)
-
-
-
-=======
-adjusted_injury_percent$injury -> if_else (adjusted_injury_percent$injury<0,0,adjusted_injury_percent$injury)
-
-#Current Issues
-#I need to figure out how to loop back to appropriate controls for each replicate, not just call to a specific cell
-#is this where I need to run a loop?
-#Yes you likely need to run a loop, dividing out each individual by the unique ID column you created
-
-#code for absolute numbers isn't working correctly; 
-#returns error Error in ifelse(injury_percent$injury < 0, 0, NA) <- injury_percent$injury : 
-#could not find function "ifelse<-"
-#the function is simply ifelse() not if_else()
-#you also had -> instead of <- which will cause the error. I corrected it in line 28 but you can see those errors in line 34.
->>>>>>> 89f3503bc424a2f0372892636ff027f4514d1eb9
->>>>>>> 4e0fee72d5c2415e77539955ceb431d8d164728a
 
 
 
