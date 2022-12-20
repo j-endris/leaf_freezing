d712ca858d8f7fc08060e6430f8973849fb97ae8
Joe Endris <joseph.endris@outlook.com>
2022-12-19 18:09:00 -0600

    update with new plots

--- cold tolerance plots.R
+++ cold tolerance plots.R
@@ -13,11 +13,11 @@ library(gridExtra)
 
 setwd("~/Library/CloudStorage/GoogleDrive-jendris@my.apsu.edu/.shortcut-targets-by-id/1p5eHgH8eX9-QjkyyA3uRz5Lk7ontMZtO/Rehm lab - General/Trees/1- Freezing/Data/")
 
-temp<-read_excel("~/Library/CloudStorage/GoogleDrive-jendris@my.apsu.edu/.shortcut-targets-by-id/1p5eHgH8eX9-QjkyyA3uRz5Lk7ontMZtO/Rehm lab - General/Trees/1- Freezing/Data/LT50 master.xlsx")
-outputs <- read.csv("LT50 practice.csv")
+outputs<-read_excel("~/Library/CloudStorage/GoogleDrive-jendris@my.apsu.edu/.shortcut-targets-by-id/1p5eHgH8eX9-QjkyyA3uRz5Lk7ontMZtO/Rehm lab - General/Trees/1- Freezing/Data/LT50 master.xlsx")
+
 
 ggplot(outputs, aes(x = Location, y = LT50, shape = Species, color = Species)) +
-  geom_boxplot(size = 2) +
+  geom_point(size = 2) +
   xlab("Location") +
   ylab("Temperature (C)") +
   theme_bw()
@@ -60,18 +60,22 @@ locations2 <- locations%>%
 g1<-ggplot(locations, aes(x = Location, y=LT50_mean, group=Species,color=Species)) +
   geom_point(position=position_dodge(0.5))+
   geom_errorbar(aes(ymax=LT50_mean+LT50_se,ymin=LT50_mean-LT50_se),position=position_dodge(0.5))+
-  ylim(-20,0) +
+  ylim(-35,-5) +
   theme(legend.position="none")
 
 g2<-ggplot(locations, aes(x = Location, y=LT15_mean, group=Species,color=Species)) +
   geom_point(position=position_dodge(0.5))+
   geom_errorbar(aes(ymax=LT15_mean+LT15_se,ymin=LT15_mean-LT15_se),position=position_dodge(0.5))+
-  ylim(-20,0)
-g3<- 
+  ylim(-35,-5) +
+  theme(legend.position="none")
+
+g3<- ggplot(locations, aes(x = Location, y=LT95_mean, group=Species,color=Species)) +
+  geom_point(position=position_dodge(0.5))+
+  geom_errorbar(aes(ymax=LT95_mean+LT95_se,ymin=LT95_mean-LT95_se),position=position_dodge(0.5))+
+  ylim(-35,-5)
   
-g1
-g2
-grid.arrange(g1,g2,nrow=1)
+grid.arrange(g1,g2,g3,nrow=1)
+
 # geom_point(aes(y=LT15_mean))+
   # geom_point(aes(y=LT50_mean))+
   # geom_point(aes(y=LT95_mean))+
@@ -85,15 +89,42 @@ outputs$Date <- mdy(outputs$Date)
 outputs_date <- group_by(outputs, Date) +
   summarise(LT15= mean(LT15), LT50=mean(LT50), LT95=mean(LT95))
 
-ggplot(outputs, aes(x= Date, color= Species)) +
-  geom_point(aes(y=LT15))+
-  geom_point(aes(y=LT50))+
-  geom_point(aes(y=LT95))+
+ggplot(outputs, aes(x= Date,y=LT50, color= Species)) +
   xlab ("Date")+
   ylab ("Temperature (C)")+
   theme_bw()
 
-###work to do
-#1 add std dev for plots
-#2 review Lim et al 1998 & Perez and Feely 2020 for other possible types of plots to create
+###########################################
+##Before and After Mean last freeze plots##
+###########################################
+
+
+#omit any blank spots in the last_freeze column
+outputs_LF <- outputs[complete.cases(outputs[,8]),]
+
+outputs_LF <- outputs_LF%>%
+  group_by(last_freeze, Location, Species)%>%
+  dplyr::summarise(across(LT15:LT95,list(mean=~mean(.),sd=~sd(.),se=~sd(./sqrt(6)))))
+
+##Grouped by location##
+BA_loc_plot <- ggplot(outputs_LF, aes(x=Location, y=LT50_mean, color=Species, last_freeze)) +
+  geom_point(position=position_dodge(0.5))+
+  geom_errorbar(aes(ymax=LT50_mean+LT50_se,ymin=LT50_mean-LT50_se), position=position_dodge(0.5))+
+  xlab ("Location") +
+  ylab ("Temperature (C)")+
+  theme_grey()
+  theme(legend.position="none")
+
+BA_species_plot <- ggplot(outputs_LF, aes(x=Species, y=LT50_mean, color=last_freeze))+
+  geom_point(position=position_dodge(0.5))+
+  geom_errorbar(aes(ymax=LT50_mean+LT50_se,ymin=LT50_mean-LT50_se), position=position_dodge(0.5))+
+  xlab ("Species") +
+  ylab ("Temperature (C)")+
+  theme_grey()
+
+BA_loc_plot
+BA_species_plot
+grid.arrange(BA_loc_plot,BA_species_plot,nrow=1)
+
+
 
