5d90dfae4dae4de06f951addb7344c3208f11268
Joe Endris <joseph.endris@outlook.com>
2023-01-06 09:40:30 -0600

    Update cold tolerance plots.R

--- cold tolerance plots.R
+++ cold tolerance plots.R
@@ -16,7 +16,7 @@ setwd("~/Library/CloudStorage/GoogleDrive-jendris@my.apsu.edu/.shortcut-targets-
 outputs<-read_excel("~/Library/CloudStorage/GoogleDrive-jendris@my.apsu.edu/.shortcut-targets-by-id/1p5eHgH8eX9-QjkyyA3uRz5Lk7ontMZtO/Rehm lab - General/Trees/1- Freezing/Data/LT50 master.xlsx")
 
 
-ggplot(outputs, aes(x = Location, y = LT50, shape = Species, color = Species)) +
+ggplot(outputs, aes(x = State, y = LT50, shape = Species, color = Species)) +
   geom_point(size = 2) +
   xlab("Location") +
   ylab("Temperature (C)") +
@@ -49,7 +49,7 @@ ggplot(thresholds, aes(x = Species, color = Species)) +
 options(dplyr.summarise.inform = FALSE)
 
 locations <- outputs%>%
-  group_by(Species, Location)%>%
+  group_by(Species, State)%>%
   dplyr::summarise(across(LT15:LT95,list(mean=~mean(.),sd=~sd(.),se=~sd(./sqrt(12)))))
 
 locations2 <- locations%>%
@@ -57,19 +57,19 @@ locations2 <- locations%>%
                names_to = "dmg_threshold",
                values_to = "values")
 
-g1<-ggplot(locations, aes(x = Location, y=LT50_mean, group=Species,color=Species)) +
+g1<-ggplot(locations, aes(x = State, y=LT50_mean, group=Species,color=Species)) +
   geom_point(position=position_dodge(0.5))+
   geom_errorbar(aes(ymax=LT50_mean+LT50_se,ymin=LT50_mean-LT50_se),position=position_dodge(0.5))+
   ylim(-35,-5) +
   theme(legend.position="none")
 
-g2<-ggplot(locations, aes(x = Location, y=LT15_mean, group=Species,color=Species)) +
+g2<-ggplot(locations, aes(x = State, y=LT15_mean, group=Species,color=Species)) +
   geom_point(position=position_dodge(0.5))+
   geom_errorbar(aes(ymax=LT15_mean+LT15_se,ymin=LT15_mean-LT15_se),position=position_dodge(0.5))+
   ylim(-35,-5) +
   theme(legend.position="none")
 
-g3<- ggplot(locations, aes(x = Location, y=LT95_mean, group=Species,color=Species)) +
+g3<- ggplot(locations, aes(x = State, y=LT95_mean, group=Species,color=Species)) +
   geom_point(position=position_dodge(0.5))+
   geom_errorbar(aes(ymax=LT95_mean+LT95_se,ymin=LT95_mean-LT95_se),position=position_dodge(0.5))+
   ylim(-35,-5)
@@ -106,11 +106,11 @@ ggplot(outputs_date, aes(x= Date,y=LT50.m, color= Species)) +
 outputs_LF <- outputs[complete.cases(outputs[,8]),]
 
 outputs_LF <- outputs_LF%>%
-  group_by(last_freeze, Location, Species)%>%
+  group_by(last_freeze, State, Species)%>%
   dplyr::summarise(across(LT15:LT95,list(mean=~mean(.),sd=~sd(.),se=~sd(./sqrt(6)))))
 
 ##Grouped by location##
-BA_loc_plot <- ggplot(outputs_LF, aes(x=Location, y=LT50_mean, color=Species, last_freeze)) +
+BA_loc_plot <- ggplot(outputs_LF, aes(x=State, y=LT50_mean, color=Species, last_freeze)) +
   geom_point(position=position_dodge(0.5))+
   geom_errorbar(aes(ymax=LT50_mean+LT50_se,ymin=LT50_mean-LT50_se), position=position_dodge(0.5))+
   xlab ("Location") +
@@ -122,7 +122,7 @@ BA_species_plot <- ggplot(outputs_LF, aes(x=Species, y=LT50_mean, color=last_fre
   geom_errorbar(aes(ymax=LT50_mean+LT50_se,ymin=LT50_mean-LT50_se), position=position_dodge(0.5))+
   xlab ("Species") +
   ylab ("Temperature (C)")+
-  facet_wrap(~Location)+
+  facet_wrap(~State)+
   theme_grey()
 
 BA_loc_plot
